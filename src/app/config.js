aa0fed58ede3fe9b5ce1f800fb9dc4a5edf7cfb0
See Guo Lin <see@guol.in>
2015-03-01 22:03:00 +0800

    Include Satellizer library for Twitter authentication.

--- src/app/config.js
+++ src/app/config.js
@@ -15,7 +15,21 @@ function config
     .state('main', {
       url: '/',
       templateUrl: 'app/main/main.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+      resolve: {
+        authenticated: function ($q, $location, $auth) {
+          var deferred = $q.defer();
+
+          if (!$auth.isAuthenticated()) {
+            $location.url('/login');
+            deferred.resolve(false);
+          } else {
+            deferred.resolve(true);
+          }
+
+          return deferred.promise;
+        }
+      }
     });
 
   $urlRouterProvider.otherwise('/');
