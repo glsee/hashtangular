f2410b4980aa8c2e2c5a78bd5a3916f1912b8a2e
See Guo Lin <see@guol.in>
2015-03-02 16:29:35 +0800

    Remove login and logout states.

--- src/app/config.js
+++ src/app/config.js
@@ -11,16 +11,6 @@ function config
   $httpProvider.defaults.headers.common['Accept']= 'application/json';
 
   $stateProvider
-    .state('login', {
-      url: '/login',
-      templateUrl: 'app/login/login.html',
-      controller: 'LoginCtrl'
-    })
-    .state('logout', {
-      url: '/logout',
-      template: null,
-      controller: 'LogoutCtrl'
-    })
     .state('main', {
       url: '/{search:[a-zA-Z0-9~_.!*\'(),%-]*}',
       templateUrl: 'app/main/main.html',
